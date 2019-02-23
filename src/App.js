eb40faaa92d2131332c7725086ce281d562c205c
John Yu <47801207+jyu-trux@users.noreply.github.com>
2019-02-23 16:40:33 -0500

    feat: new state (#3)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {};
   render() {
-    // hello world
     return (
       <div className="App">
         <header className="App-header">
