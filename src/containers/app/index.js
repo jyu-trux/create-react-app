6b94c799ead4b3d5ac5523727117718d46e69fdb
John Yu <jyu@truxnow.com>
2019-03-09 15:46:29 -0500

    refractor: created new folder structure

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './Login';
+import './app.css';
+import Login from '../login';
+
 class App extends Component {
   render() {
     return (
